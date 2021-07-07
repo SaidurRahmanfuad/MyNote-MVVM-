4bebe859676b5e7aa6d67b66f249182df5dcd614
Saidur Rahman <devsaidur@gmail.com>
2021-07-08 00:40:38 +0600

    Add viewbinding,Insert Note Layout design

--- app/src/main/java/com/saidur/mynote/MainActivity.java
+++ app/src/main/java/com/saidur/mynote/MainActivity.java
@@ -2,13 +2,31 @@ package com.saidur.mynote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
+import com.saidur.mynote.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding amb;
+    Dialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        amb = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(amb.getRoot());
+        //setContentView(R.layout.activity_main);
+
+
+        initpopup();
+        amb.addnotebtn.setOnClickListener(v -> {
+
+        });
+    }
+
+    private void initpopup() {
+        dialog.setContentView(R.layout.layout_insertnote);
+
     }
 }
